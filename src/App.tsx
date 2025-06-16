b9841cbaba7074f1f3d51fede24a3dc9f599b1e9
Kate <gutkati@email.ru>
2025-06-16 10:58:13 +0400

    Настроить валидацию формы

--- src/App.tsx
+++ src/App.tsx
@@ -6,13 +6,22 @@ import {Container, Heading} from '@chakra-ui/react';
 function App() {
     const [seq1, setSeq1] = useState('');
     const [seq2, setSeq2] = useState('');
+    
+    function changeSubmitAlign(s1: string, s2: string) {
+        setSeq1(s1)
+        setSeq2(s2)
+    }
     return (
         <Container maxW='container.md' p={4}>
             <Heading textAlign='center' mb={6}>
                 Анализ аминокислотных последовательностей
             </Heading>
-            <AlignForm/>
-            <AlignmentDisplay/>
+            <AlignForm
+                seq1={seq1}
+                seq2={seq2}
+                onSubmitAlign={changeSubmitAlign}
+            />
+            <AlignmentDisplay />
         </Container>
     );
 }
--- src/components/AlignForm.tsx
+++ src/components/AlignForm.tsx
@@ -1,22 +1,81 @@
 import React, {useState} from 'react';
 import {useForm} from 'react-hook-form';
-import {Box, Button, Input, FormControl, FormLabel, VStack} from '@chakra-ui/react';
+import {Box, Button, Input, FormControl, FormLabel, FormErrorMessage, VStack} from '@chakra-ui/react';
 
-const AlignForm = () => {
+type FormData = {
+    seq1: string;
+    seq2: string;
+}
+
+interface AlignFormProps {
+    seq1: string;
+    seq2: string;
+    onSubmitAlign: (seq1: string, seq2: string) => void;
+}
+
+const AlignForm: React.FC<AlignFormProps> = ({seq1, seq2, onSubmitAlign}) => {
+
+    const {
+        register,
+        handleSubmit,
+        watch,
+        formState: {errors},
+    } = useForm<FormData>({
+        defaultValues: {
+            seq1: seq1 || '',
+            seq2: seq2 || '',
+        }
+    })
+
+    const onSubmit = (data: FormData) => {
+        onSubmitAlign(data.seq1.toUpperCase(), data.seq2.toUpperCase());
+    };
+
+    const validateSequence = (value: string) =>
+        /^[ARNDCEQGHILKMFPSTWYV-]+$/i.test(value) || 'Только допустимые символы (A, R, N, ...)'
+
+
+    const validateLengthMatch = (value: string) => {
+        const other = watch('seq1');
+        return value.length === other.length || 'Последовательности должны быть одинаковой длины';
+    }
+
     return (
-        <Box maxW='600px' mx='auto' p={4}>
-            <VStack>
-                <FormControl>
-                    <FormLabel>
-                        <Input/>
-                    </FormLabel>
+        <Box
+            as='form'
+            onSubmit={handleSubmit(onSubmit)}
+            maxW='600px'
+            mx='auto'
+            p={4}>
+
+            <VStack spacing={4}>
+                <FormControl isInvalid={!!errors.seq1}>
+                    <FormLabel>Последовательность 1</FormLabel>
+                    <Input
+                        placeholder='VLSPADKTNIKASWEKIGSHG'
+                        {...register('seq1', {
+                            required: 'Обязательное поле',
+                            validate: validateSequence,
+                        })}
+                    />
+                    <FormErrorMessage>{errors.seq1?.message}</FormErrorMessage>
                 </FormControl>
-                <FormControl>
-                    <FormLabel>
-                        <Input/>
-                    </FormLabel>
+
+                <FormControl isInvalid={!!errors.seq2}>
+                    <FormLabel>Последовательность 2</FormLabel>
+                    <Input
+                        placeholder='VLSPADKTNIKASWEKIGSHG'
+                        {...register('seq2', {
+                            required: 'Обязательное поле',
+                            validate: {
+                                format: validateSequence,
+                                length: validateLengthMatch,
+                            },
+                        })}
+                    />
+                    <FormErrorMessage>{errors.seq2?.message}</FormErrorMessage>
                 </FormControl>
-                <Button>Выравнить</Button>
+                <Button colorScheme="teal" type="submit" w="full">Выравнить</Button>
             </VStack>
         </Box>
     );
